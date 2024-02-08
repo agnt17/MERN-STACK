92c70904c6eb80cf49b2e3888d1c1d63bbc5ac5c
frozenopertor <adityagupta11112002@gmail.com>
2024-02-08 13:23:14 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 import express from "express";
-
+import mongoose, { mongo } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
+mongoose.connect(process.env.MONGO).then(()=>{console.log("Mongodb initial connection successfull")}).catch((error) => {console.log(`Error found ${error}`)})
 app.listen(3000, () => {
   console.log(`Server is listening on port 3000!`);
 });
