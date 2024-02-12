11037a3ff785f4e14fa04d29daa38b9737a213a8
frozenopertor <adityagupta11112002@gmail.com>
2024-02-12 20:37:52 +0530

    signup page UI developed

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,11 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+// import { errorHandler } from '../utils/error.js/';
+// import jwt from 'jsonwebtoken';
+
 export const signup = async (req, res, next) => {
-  // this is done to save the data from the api route to the database
   const { username, email, password } = req.body;
-  const hashedPassword = bcryptjs.hashSync(password, 10); // using this passsword get encrypted
+  const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
--- api/index.js
+++ api/index.js
@@ -1,35 +1,51 @@
-import express from "express";
-import mongoose, { mongo } from "mongoose";
-import dotenv from "dotenv";
-import userRouter from "./routes/user.route.js";
-import { signup } from "./controllers/auth.controller.js";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+// import listingRouter from './routes/listing.route.js';
+// import cookieParser from 'cookie-parser';
+// import path from 'path';
 dotenv.config();
-const app = express();
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("Mongodb initial connection successfull");
+    console.log('Connected to MongoDB!');
   })
-  .catch((error) => {
-    console.log(`Error found ${error}`);
+  .catch((err) => {
+    console.log(err);
   });
+
+  // const __dirname = path.resolve();
+
+const app = express();
 
 app.use(express.json());
 
+// app.use(cookieParser());
+
 app.listen(3000, () => {
-  console.log(`Server is listening on port 3000!`);
+  console.log('Server is running on port 3000!');
 });
 
-app.get("/test", (req, res) => {
-  res.json({ message: "this is test message" });
-});
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+// app.use('/api/listing', listingRouter);
 
-app.use("/api/user", userRouter); // this is used to use the created userRoute.
-app.use("/api/auth", signup);
+
+// app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
-  return res.status(statusCode).json({ success: false, statusCode, message });
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
